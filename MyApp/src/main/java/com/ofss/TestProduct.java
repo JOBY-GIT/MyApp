d9daae765cc058ea9b4a8780c85106dcf8c278a4
Joby <jobythomask@gmail.com>
2020-08-06 14:40:11 +0530

    Push2

--- MyApp/src/main/java/com/ofss/TestProduct.java
+++ MyApp/src/main/java/com/ofss/TestProduct.java
@@ -5,7 +5,7 @@ public class TestProduct {
 	public static void main(String[] args) {
 		Product p=new Product("SmartPhone",45000);
 		System.out.println(p.getProductName()+" and "+p.getProductPrice());
-
+		System.out.println("Push1");
 	}
 
 }
